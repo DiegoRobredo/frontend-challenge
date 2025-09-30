255bf7d00cad340785b4937a716771f9f9646ed1
Diego Robredo <drobredo@gmail.com>
2025-09-30 19:35:24 +0200

    Load endpoints from env

---
+++ src/server/endpoints.ts
@@ -0,0 +1,2 @@
+export const WS_URL = `ws://${import.meta.env.VITE_SERVER_HOST}:${import.meta.env.VITE_SERVER_PORT}/notifications`
+export const DOCUMENTS_API_URL = `http://${import.meta.env.VITE_SERVER_HOST}:${import.meta.env.VITE_SERVER_PORT}/documents`
